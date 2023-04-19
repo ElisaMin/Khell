1d57b5695c20ec31fc2a180682531afaf142650f
Heizi <xm.zhongjiaxin@gmail.com>
2023-04-19 09:59:49 +0800

    log update std err and get tag from class

--- khell-log/build.gradle.kts
+++ khell-log/build.gradle.kts
@@ -1,64 +1,43 @@
-import me.heizi.koltinx.version.props
-import me.heizi.koltinx.version.versions
-
 plugins {
     id("com.android.library")
     kotlin("multiplatform")
     `maven-publish`
 }
-
+dependencies {
+    androidTestImplementation(libs.testng) {
+        exclude("org.hamcrest","hamcrest-core")
+    }
+}
 
 
 kotlin {
+//    jvmToolchain(19)
     jvm()
-    android {
-        publishLibraryVariants("release", "debug")
-    }
+    android {with(project.android) {
+//        publishLibraryVariants("release")
+        namespace = "me.heizi.kotlinx.logger"
+        compileSdk = 33
+        defaultConfig {
+            targetSdk = 33
+            minSdk = 21
+        }
+    }}
     sourceSets {
-        val commonMain by getting
-        val jvmMain by getting {
+        commonTest {
             dependencies {
-                api("org.slf4j:slf4j-api:${versions["slf4j"]}")
+                implementation(kotlin("test"))
             }
         }
-        val jvmTest by getting {
+        commonTest {
             dependencies {
-                implementation("org.slf4j:slf4j-log4j12:${versions["slf4j"]}")
+                implementation(libs.kotlinx.coroutines.core.common)
+                implementation(libs.mockk)
+                implementation(libs.slf4j.log4j12)
             }
         }
-        val androidMain by getting {
-
-
+        
+        getByName("jvmMain").dependencies {
+            implementation(libs.slf4j.api)
         }
     }
 }
-android {
-    namespace = "me.heizi.kotlinx.logger"
-    compileSdk = 33
-    defaultConfig {
-        minSdk = 21
-    }
-}
-
-
-group = "me.heizi.kotlinx"
-version = versions["khell"]
-
-
-
-//apply(rootProject.file("publishing.gradle.kts"))
-
-
-dependencies {
-
-}
-
-
-//dependencies {
-//    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-//    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
-//}
-//
-//tasks.getByName<Test>("test") {
-//    useJUnitPlatform()
-//}
---
+++ khell-log/src/commonTest/kotlin/test.kt
@@ -0,0 +1,38 @@
+package me.heizi.kotlinx.logger
+
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class LoggerTest {
+    @Test
+    fun log():Unit = runBlocking {
+        this.println("log_normal")
+        this.debug("log_normal")
+        this.error("log_normal")
+        "test".println("log_normal")
+        "test".debug("log_normal")
+        "test".error("log_normal")
+        println("log_normal","log_normal")
+        debug("log_normal","log_normal")
+        error("log_normal","log_normal")
+        launch {
+            println("log_normal","log_normal")
+            debug("log_normal","log_normal")
+            error("log_normal","log_normal")
+            launch {
+                println("log_normal","log_normal")
+                debug("log_normal","log_normal")
+                error("log_normal","log_normal")
+            }
+            launch {
+                println("log_normal","log_normal")
+                debug("log_normal","log_normal")
+                error("log_normal","log_normal")
+            }
+        }
+    }
+}
